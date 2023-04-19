06d8ac397e4b44a5d2539ca3cbd2ed321e5c3075
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-04-19 10:38:37 +0700

    create auth bearer token by postman

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\User\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,17 @@ Route::get('test', function () {
     return response('This is working', 200);
 });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+
+    Route::get('inside-middleware', function() {
+        return response()->json('Success', 200);
+    });
 });
